efd987c9058114a6d26da9b2bd465ff50eb31a61
VeraV3 <veravukosavljevic0@gmail.com>
2023-02-22 00:14:27 +0100

    sservlet changed but doesn't work

--- src/main/java/servers/ServletSubmit.java
+++ src/main/java/servers/ServletSubmit.java
@@ -42,9 +42,9 @@ public class ServletSubmit extends HttpServlet{
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException{
         
         res.setContentType("text/html");
-        //PrintWriter pw = res.getWriter();
-        //pw.println("<h2>uspesno je dodat korisnik</h2>");
-        System.out.println("u serveru sam, u metodu service");
+        PrintWriter pw = res.getWriter();
+        pw.println("<h2>u serveru sam, u metodu service</h2>");
+        //System.out.println("u serveru sam, u metodu service");
 
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
@@ -62,7 +62,7 @@ public class ServletSubmit extends HttpServlet{
         putanja.setReadable(true);
 
         try {
-            load(user, putanja);
+            load(user, putanja, pw);
         } catch (Exception e1) { 
             e1.printStackTrace();
             System.out.println("Exception in puting user to file");
@@ -88,7 +88,7 @@ public class ServletSubmit extends HttpServlet{
                 return true;	
             }
         }
-        System.out.println("Continent doesnt exist!");
+        //System.out.println("Continent doesnt exist!");
         return false;
     }
     //TODO obrisi ispis ovde
@@ -96,22 +96,45 @@ public class ServletSubmit extends HttpServlet{
         NodeList adrese = doc.getElementsByTagName("country");
         for(int x = 0; x< adrese.getLength(); x++) {
             if(adrese.item(x).getTextContent().equals(user.getCountry())) {
-                System.out.println("drzava vec postoji");
+                //System.out.println("drzava vec postoji");
                 return true;	
             }
         }
-        System.out.println("Drzava postoji");
+        //System.out.println("Drzava ne postoji");
         return false;
     }
     
-    static boolean check(Document doc, User user){
+    static boolean isDuplicate(Document doc, User user){
         NodeList addresses = doc.getElementsByTagName("email");
         for(int x = 0; x< addresses.getLength(); x++) {
-            if(addresses.item(x).getTextContent().equals(user.getEmail())) {
-                return true;	
+            if(addresses.item(x).getTextContent().equals(user.getEmail())) { 	
+                //System.out.println("Nasla sam istu adresu i ona glasi: " + addresses.item(x).getTextContent());
+                return true;
             }
         }
         return false;
+    }
+
+    static Node findContinent(Document doc, User user, Node root){
+        Node continent = null;
+        NodeList maybeContinentList = root.getChildNodes();
+                for (int i = 0; i < maybeContinentList.getLength(); i++) {
+                // System.out.println("1. nivo	" + i);
+                    Node maybeContinent = maybeContinentList.item(i);
+                    if(maybeContinent.getNodeName() == "continent") {
+                         NamedNodeMap nm = maybeContinent.getAttributes();
+                        for (int j = 0; j < nm.getLength(); j++) {
+                        //System.out.println("Prolazim kroz atribute cvora " + i + " i redni broj atributa je " + j);
+                             Attr node =  (Attr)nm.item(j);
+                            if(node.getNodeValue().equals(user.getContinent())) {
+                                return maybeContinent;
+                                
+                            }
+                        }
+                    }
+                }
+            
+        return continent;
     }
     //TODO obrisati ispis
     //TODO dodati beline za strukturiranje xml-a
@@ -135,7 +158,7 @@ public class ServletSubmit extends HttpServlet{
         newUser.appendChild(ncity);
         newUser.appendChild(nemail);
         newUser.appendChild(npassword);
-        System.out.println("Novii korisnik je napravljen");
+        //System.out.println("Novi korisnik je napravljen");
         return newUser;    
     }
 
@@ -156,61 +179,73 @@ public class ServletSubmit extends HttpServlet{
         return newContinent;	
     }
     //TODO optimizacija
-    static void load(User user, File path) throws TransformerFactoryConfigurationError, ParserConfigurationException, SAXException, IOException, TransformerException {
+    static void load(User user, File path, PrintWriter pw) throws TransformerFactoryConfigurationError, ParserConfigurationException, SAXException, IOException, TransformerException {
         DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder domBuilder = domFactory.newDocumentBuilder();
         Document doc = domBuilder.parse(path);
         Node root = doc.getDocumentElement();
-        boolean indikatorDuplikata = check(doc, user) ;
-        if(indikatorDuplikata==false) {	
-            NodeList maybeContinentList = root.getChildNodes();
-            for (int i = 0; i < maybeContinentList.getLength(); i++) {
-                // System.out.println("1. nivo	" + i);
-                Node maybeContinent = maybeContinentList.item(i);
-                if(maybeContinent.getNodeName() == "continent") {
-                    NamedNodeMap nm = maybeContinent.getAttributes();
-                    for (int j = 0; j < nm.getLength(); j++) {
-                        //System.out.println("Prolazim kroz atribute cvora " + i + " i redni broj atributa je " + j);
-                        Attr node =  (Attr)nm.item(j);
-                        if(node.getNodeValue().equals(user.getContinent())) {
-                            NodeList maybeCountryList = maybeContinent.getChildNodes();
-                            for(int k = 0; k<maybeCountryList.getLength(); k++) {
-                                //System.out.println("	2. nivo		" + k);
-                                Node maybeCountry = maybeCountryList.item(k);
-                                if(maybeCountry.getNodeName() == "country") {
-                                    NamedNodeMap nm2 = maybeCountry.getAttributes();
-                                    for (int d = 0; d < nm2.getLength(); d++) {
-                                        //	System.out.println("		3. nivo		" + d);
-                                        Attr node2 =  (Attr)nm2.item(d);
-                                        if(node2.getNodeValue().equals(user.getCountry())) {
-                                            Element newUser = newLeaf(doc, user);
-                                            maybeCountry.appendChild(newUser);
-                                            
-                                        }
-                                    }
-                                }
-                            }
-                            if(!countryExists(doc, user) && continentExists(doc, user)  ) { //!check(doc, user) &&  u uslovu
-                                 System.out.println("Drzava ne postoji, kontinent postoji, treba da napravim drzavu");
-                                Element newCountry = newCountryNode(doc, user);
-                                maybeContinent.appendChild(newCountry);
-                            }
-                                
-                        }
-                                  
-                    }
-                }
-             
-            }
+       // boolean indikatorDuplikata = check(doc, user) ;
+        if(!isDuplicate(doc, user)) {	
             if(!continentExists(doc, user)) { // && !check(doc, user) u uslovu
-                System.out.println("Kontinent ne postoji, treba da napravim kontinent");
+                pw.println("<h2>Kontinent ne postoji, treba da napravim kontinent</h2>");
+                //System.out.println("Kontinent ne postoji, treba da napravim kontinent");
                 Element newContinent = newContinentNode(doc, user);
                 root.appendChild(newContinent);
                                     
             }
+            else if(!countryExists(doc, user)) { //!check(doc, user) &&  u uslovu
+                pw.println("<h2>Drzava ne postoji, kontinent postoji, treba da napravim drzavu</h2>");
+
+               //System.out.println("Drzava ne postoji, kontinent postoji, treba da napravim drzavu");
+               Element newCountry = newCountryNode(doc, user);
+               Node cont = findContinent(doc, user, root);
+               cont.appendChild(newCountry);
+           }else{
+                NodeList maybeContinentList = root.getChildNodes();
+                for (int i = 0; i < maybeContinentList.getLength(); i++) {
+                // System.out.println("1. nivo	" + i);
+                    Node maybeContinent = maybeContinentList.item(i);
+                    if(maybeContinent.getNodeName() == "continent") {
+                         NamedNodeMap nm = maybeContinent.getAttributes();
+                        for (int j = 0; j < nm.getLength(); j++) {
+                        //System.out.println("Prolazim kroz atribute cvora " + i + " i redni broj atributa je " + j);
+                             Attr node =  (Attr)nm.item(j);
+                            if(node.getNodeValue().equals(user.getContinent())) {
+                                 NodeList maybeCountryList = maybeContinent.getChildNodes();
+                                for(int k = 0; k<maybeCountryList.getLength(); k++) {
+                                //System.out.println("	2. nivo		" + k);
+                                     Node maybeCountry = maybeCountryList.item(k);
+                                    if(maybeCountry.getNodeName() == "country") {
+                                        NamedNodeMap nm2 = maybeCountry.getAttributes();
+                                        for (int d = 0; d < nm2.getLength(); d++) {
+                                        //	System.out.println("		3. nivo		" + d);
+                                            Attr node2 =  (Attr)nm2.item(d);
+                                            if(node2.getNodeValue().equals(user.getCountry())) {
+                                                Element newUser = newLeaf(doc, user);
+                                                maybeCountry.appendChild(newUser);
+                                            
+                                            }
+                                        }
+                                    }
+                                }
+                            
+                                
+                            }
+                                  
+                        }
+                    }
+             
+                }  
+            }
+
         }
         else{
-            System.out.println("User already exists!");
+            pw.println("<h2>User already exists!</h2>");
+
+            //System.out.println("User already exists!");
+            pw.println("<h2>" + user.toString()+ "</h2>");
+
+            //System.out.println(user);
            
         }
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
